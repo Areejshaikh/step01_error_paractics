29eb804568c59e8267967d8d323ca2d49e491deb
Areejshaikh <160318564+Areejshaikh@users.noreply.github.com>
2024-05-16 00:25:16 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+"use strict";
+//  // Syntax Error
+//   let arr :number[] =[1,2,3
+//      for (const array of arr) {
+//         console.log(array);
+//  }
+//  missing closing bracket "]" and parentheses" ) "
+// // Type Error
+//  interface PersonData {
+//     name :string;
+//     age : number;
+//  }
+//  let  person : PersonData ={
+//     name : "jhon",
+//  the "age" property is missing in the object
+//  }
+//  // assignability_error
+let fruits = ["Apple", "Kiwi", "Mongo"];
+// fruits = [1,2,3,]  // assignability_error
+console.log(fruits);
---
+++ index.ts
@@ -0,0 +1,27 @@
+//  // Syntax Error
+//   let arr :number[] =[1,2,3
+//      for (const array of arr) {
+//         console.log(array);
+    //  }
+    //  missing closing bracket "]" and parentheses" ) "
+
+
+
+     // // Type Error
+
+    //  interface PersonData {
+    //     name :string;
+    //     age : number;
+    //  }
+    //  let  person : PersonData ={
+    //     name : "jhon",
+    //  the "age" property is missing in the object
+    //  }
+//  // assignability_error
+
+
+let fruits : string[] = ["Apple","Kiwi","Mongo"];
+// fruits = [1,2,3,]  // assignability_error
+console.log(fruits);
+
+  
